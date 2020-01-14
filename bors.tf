e8c6a5cb16be44359cc4411d49c37c5c0d68e990
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2020-01-14 23:00:17 +0000

    show url in output

--- bors.tf
+++ bors.tf
@@ -94,3 +94,7 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
+
+output "url" {
+  value = google_cloud_run_service.bors.status[0].url
+}
