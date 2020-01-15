499195b1fb2a3dcdd62042571a7ba41d42bbdb08
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2020-01-15 21:16:51 +0000

    give up and debug locally

--- bors.sh
+++ bors.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ANSIBLE_VENV="$HOME/.venv/ansible"
+BASE_VENV="$HOME/.venv"
+ORIGINAL_PATH="$PATH"
+SALT_STATES="${HOME}/z/fm/anr"
 
 function get_os_id() {
     cat /etc/os-release | grep -E '^ID=' | awk -F = '{print $2}'
@@ -26,19 +28,30 @@ function apt_install() {
     maybe_sudo apt install -y git python3-virtualenv
 }
 
-function activate_ansible_venv() {
-    source "${ANSIBLE_VENV}/bin/activate"
+function activate_venv() {
+    venv="$1"
+
+    venv_bin="${BASE_VENV}/${venv}/bin"
+    export PATH="${venv_bin}:$PATH"
+    export VIRTUAL_ENV="${BASE_VENV}/venv"
+}
+
+function deactivate_venv() {
+    export PATH="$ORIGINAL_PATH"
+    unset VIRTUAL_ENV
 }
 
 function venv_install_ansible() {
-    virtualenv "${ANSIBLE_VENV}"
-    activate_ansible_venv
+    virtualenv --python $(which python3) "${BASE_VENV}/ansible"
+    activate_venv ansible
     pip install ansible
+    deactivate_venv ansible
 }
 
 function ansible_pull() {
-    activate_ansible_venv
-    ansible_pull -U https://github.com/femnad/casastrap.git -i "$(hostname)," salt-init.yml
+    activate_venv ansible
+    inventory="$(hostname),"
+    ansible-pull -U https://github.com/femnad/casastrap.git -i "$inventory" init-salt.yaml
 }
 
 function install_packages() {
@@ -56,7 +69,15 @@ function install_packages() {
         exit 1
         ;;
     esac
+}
 
+function salt_apply() {
+    activate_venv salt
+    pushd "$SALT_STATES"
+    salt-ssh self-sudo state.apply all
+    salt-ssh self state.apply all
+    deactivate_venv salt
+    popd
 }
 
 function main() {
