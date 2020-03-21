a1d2572a542773c608424726e910b2626861b135
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2020-03-21 12:54:43 +0000

    get routes from a file

--- bors.go
+++ bors.go
@@ -2,42 +2,80 @@ package main
 
 import (
 	"fmt"
-	"github.com/alexflint/go-arg"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/alexflint/go-arg"
+	"gopkg.in/yaml.v3"
 )
 
 var args struct {
-	Port int    `arg:"env" default:"8080"`
-	File string `arg:"required,-f"`
+	Port int    `arg:"env" default:"8080" help:"port to serve on"`
+	File string `arg:"required,-f" help:"routes file"`
 }
 
-func panicOnErr(err error) {
-	if err != nil {
-		panic(err)
-	}
+type routeMapping struct {
+	Route string
+	File string
 }
-func main() {
-	arg.MustParse(&args)
-	log.Printf("Serving %s on port %d", args.File, args.Port)
 
-	serve := func(w http.ResponseWriter, r *http.Request) {
-		fileBytes, err := ioutil.ReadFile(args.File)
+type routeMappingSpec struct {
+	Routes []routeMapping
+}
+
+func getHandler(file string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fileBytes, err := ioutil.ReadFile(file)
 		if err != nil {
 			log.Printf("error reading file contents: %s", err)
+			w.WriteHeader(500)
 		}
 		numBytes, err := w.Write(fileBytes)
 		if err != nil {
 			log.Printf("error writing response body: %s", err)
+			w.WriteHeader(500)
 		}
 		log.Printf("Wrote %d bytes", numBytes)
 	}
-	http.HandleFunc("/", serve)
+}
+
+func main() {
+	arg.MustParse(&args)
+
+	routesFile := args.File
+
+	f, err := os.Open(routesFile)
+	if err != nil {
+		log.Fatalf("Error opening route spec file %s: %v", routesFile, err)
+	}
+	defer f.Close()
+
+	routeSpecContent, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatalf("Error reading routes file %s: %v", routesFile, err)
+	}
+
+	var spec routeMappingSpec
+
+	err = yaml.Unmarshal(routeSpecContent, &spec)
+	if err != nil {
+		log.Fatalf("Error deserializing routes file %s: %v", routesFile, err)
+	}
+
+	log.Printf("Serving routes from %s on port %d", routesFile, args.Port)
+
+	for _, route := range spec.Routes {
+		routeFn := getHandler(route.File)
+		http.HandleFunc(route.Route, routeFn)
+	}
 
 	addr := fmt.Sprintf(":%d", args.Port)
-	err := http.ListenAndServe(addr, nil)
-	panicOnErr(err)
+	err = http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatalf("Error serving HTTP: %v", err)
+	}
 
 	log.Println("Stopped serving")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/femnad/bors
 
 go 1.13
 
-require github.com/alexflint/go-arg v1.2.0
+require (
+	github.com/alexflint/go-arg v1.2.0
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
+)
