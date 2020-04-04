344f7e10e8ed35cd703cd5da4b477de76c5f7b0e
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2020-04-04 17:48:07 +0100

    enable kubectl bash completion

--- contrib/b8s.sh
+++ contrib/b8s.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ALIASES_FILE="${HOME}/.bash_aliases"
+BASHRC="${HOME}/.bashrc"
 VIMRC="${HOME}/.vimrc"
 
 cat > "$ALIASES_FILE" << EOF
@@ -9,6 +10,9 @@ alias kd='kubectl delete'
 alias kds='kubectl describe'
 alias kdsn='kubectl describe node'
 alias kdp='kubectl delete pod'
+alias ke='kubectl edit'
+alias ked='kubectl edit deployment'
+alias kep='kubectl edit pod'
 alias kgdy='kubectl get deployment -o yaml'
 alias kdsp='kubectl describe pod'
 alias kgcm='kubectl get configmap'
@@ -16,6 +20,7 @@ alias kgcmy='kubectl get configmap -o yaml'
 alias kgd='kubectl get deployment'
 alias kg='kubectl get'
 alias kgn='kubectl get nodes'
+alias kgns='kubectl get ns'
 alias kgp='kubectl get pod'
 alias kgpy='kubectl get pod -o yaml'
 alias kgpan='kubectl get pod --all-namespaces'
@@ -25,21 +30,28 @@ alias kgsay='kubectl get sa -o yaml'
 alias kgsc='kubectl get secret'
 alias kgscy='kubectl get secret -o yaml'
 alias kl='kubectl label'
+alias kt='kubectl top'
+alias ktn='kubectl top node'
+alias ktp='kubectl top pod'
 alias v=vim
 EOF
 
 cat > "$VIMRC" << EOF
 set nocompatible
+set backspace=indent,eol,start
 set hlsearch
 set ignorecase
 set incsearch
 set number
 syntax on
 filetype plugin indent on
-set tabstop=4
-set shiftwidth=4
-set expandtab
+set tabstop=2
+set shiftwidth=2
+set softtabstop=2
 set nrformats-=octal
 EOF
+
+echo 'source <(kubectl completion bash)' >> "$BASHRC"
+echo 'complete -F __start_kubectl k' >> "$BASHRC"
 
 bash
