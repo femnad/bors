7cada1178092bd1ddc95a02f38c90df1f6d5b358
fcd <git@fcd.dev>
2021-04-29 23:52:59 +0100

    init terraform when necessary

--- bor.sh
+++ bor.sh
@@ -4,10 +4,17 @@ set -euEo pipefail
 function bor() {
     root_dir=$(dirname $(realpath $0))
     pushd "${root_dir}"
+
+    if ! [ -d .terraform/ ]
+    then
+        make
+    fi
+
     terraform apply -auto-approve
     ansible-playbook bors.yml
-    popd
     terraform destroy -auto-approve
+
+    popd
 }
 
 bor $@
